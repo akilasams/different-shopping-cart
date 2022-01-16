f9faa687bde40ae28235e3907051f771f576a637
Akila Samarawickrama <akilaishere@gmail.com>
2022-01-16 11:15:13 +0530

    Screnario 1 & 2 Done

---
+++ server/cart/cart.js
@@ -0,0 +1,62 @@
+const findItem = (shop, itemId) => {
+  const item = shop.find((item) => item.itemId === itemId);
+  return item;
+};
+
+const addItem = (shop, cart, itemId, quantity) => {
+  const itemInShop = findItem(shop, itemId);
+  const itemInCart = findItem(cart, itemId);
+
+  if (itemInCart == null) {
+    const cartItem = { ...itemInShop, quantity: quantity };
+    cart.push(cartItem);
+  } else {
+    for (var i in cart) {
+      if (cart[i].itemId === itemId) {
+        cart[i].quantity += quantity;
+        break;
+      } else continue;
+    }
+  }
+};
+
+const quantityInCart = (cart) => {
+  var totalQuantity = 0;
+  cart.forEach((item) => {
+    totalQuantity += item.quantity;
+  });
+  return totalQuantity;
+};
+
+const calculateTotalTax = (cart, taxRate) => {
+  var totalTax = 0;
+  cart.forEach((item) => {
+    totalTax = item.unitPrice * item.quantity * taxRate;
+  });
+  var result = Number.parseFloat(totalTax.toFixed(2));
+  return result;
+};
+
+const calculateTotal = (cart) => {
+  var total = 0;
+  cart.forEach((item) => {
+    total += item.unitPrice * item.quantity;
+  });
+  var result = Number.parseFloat(total.toFixed(2));
+  return result;
+};
+
+const calculateTotalWithTax = (cart, taxRate) => {
+  var total = calculateTotal(cart);
+  var totalTax = calculateTotalTax(cart, taxRate);
+  var result = total + totalTax;
+  return result;
+};
+
+module.exports = {
+  findItem,
+  addItem,
+  quantityInCart,
+  calculateTotal,
+  calculateTotalWithTax,
+};
--- server/test/cartTest.js
+++ server/test/cartTest.js
@@ -1,9 +1,89 @@
-const cart = require('../middleware/cart');
+const expect = require('chai').expect;
+
+const cart = require('../cart/cart');
+
+const shop = [
+  {
+    itemId: 1,
+    itemName: 'Shower Gel',
+    unitPrice: 49.99,
+  },
+  {
+    itemId: 2,
+    itemName: 'Deodrant',
+    unitPrice: 99.99,
+  },
+];
 
 const SALES_TAX_RATE = 0.125;
 
-const shoppingCart = [];
+let shoppingCart = [];
 
-it('Total Tax should return 37.50', () => {
-  let result = cart.calculateTax();
+describe('Cart Functionality', () => {
+  it('Item should should not return a null', () => {
+    let result = cart.findItem(shop, 001);
+    expect(result).to.not.be.null;
+  });
+});
+
+//==================================================================
+
+describe('1) Scenario 1', () => {
+  it('Have an empty shopping cart', () => {
+    shoppingCart = [];
+    expect(shoppingCart.length).to.equal(0);
+  });
+
+  it('Add 5 shower gels to cart should return quantity of 5 in the cart', () => {
+    cart.addItem(shop, shoppingCart, 1, 5);
+    expect(cart.quantityInCart(shoppingCart)).to.equal(5);
+  });
+
+  it('Total of the items in cart should return 249.95', () => {
+    expect(cart.calculateTotal(shoppingCart)).to.equal(249.95);
+  });
+});
+
+describe('2) Scenario 2', () => {
+  it('Have an empty shopping cart', () => {
+    shoppingCart = [];
+    expect(shoppingCart.length).to.equal(0);
+  });
+
+  it('Add 5 shower gels to cart should return quantity of 5 in the cart', () => {
+    cart.addItem(shop, shoppingCart, 1, 5);
+    expect(cart.quantityInCart(shoppingCart)).to.equal(5);
+  });
+
+  it('Add 3 more shower gels to cart should return quantity of 8 in the cart', () => {
+    cart.addItem(shop, shoppingCart, 1, 3);
+    expect(cart.quantityInCart(shoppingCart)).to.equal(8);
+  });
+
+  it('Total of the items in cart should return 399.92', () => {
+    expect(cart.calculateTotal(shoppingCart)).to.equal(399.92);
+  });
+});
+
+describe('3) Scenario 3', () => {
+  it('Have an empty shopping cart', () => {
+    shoppingCart = [];
+    expect(shoppingCart.length).to.equal(0);
+  });
+
+  it('Add 2 shower gels to cart should return quantity of 2 in the cart', () => {
+    cart.addItem(shop, shoppingCart, 1, 2);
+    expect(cart.quantityInCart(shoppingCart)).to.equal(2);
+  });
+
+  it('Add 2 deodrants to cart should return quantity of 4 in the cart', () => {
+    cart.addItem(shop, shoppingCart, 2, 2);
+    expect(cart.quantityInCart(shoppingCart)).to.equal(4);
+  });
+
+  it('Total of the items in cart should return 337.46', () => {
+    expect(cart.calculateTotalWithTax(shoppingCart, SALES_TAX_RATE)).to.equal(
+      337.46
+    );
+  });
 });
